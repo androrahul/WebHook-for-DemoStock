c81d8103250b0064cbba288d5083b72d5d7b373e
androrahul <39151779+androrahul@users.noreply.github.com>
2018-05-10 17:15:37 +0530

    port change

--- webhook.py
+++ webhook.py
@@ -32,6 +32,6 @@ def makeResponse(res):
 	}
 	
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+    port = int(os.getenv('PORT', 3000))
     print("Starting app on port %d" % port)
     app.run(debug=False, port=port, host='0.0.0.0')
