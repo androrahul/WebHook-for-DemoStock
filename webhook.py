687301af851e1849af0b6f7e2feb892f39b2d492
androrahul <39151779+androrahul@users.noreply.github.com>
2018-05-10 15:05:50 +0530

    Changes 1.0

--- webhook.py
+++ webhook.py
@@ -6,19 +6,21 @@ from flask import Flask
 from flask import request
 from flask import make_response
 
-# Flask app should start in globle layout
+# Flask app should start in global layout
 app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-	req = request.get_json(slient=True, force=True)
-	print(json.dumps(req, indent=4))
-	
-	res = makeResponse(req)
-	res = json.dumps(res, indent=4)
-	r = make_response(res)
-	r.headers['Content-Type'] = "application/json"
-	return req
+    req = request.get_json(silent=True, force=True)
+    print(json.dumps(req, indent=4))
+    
+    res = makeResponse(req)
+    
+    res = json.dumps(res, indent=4)
+    # print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return r
 	
 def makeResponse(res):
 	result = res.get('result');
@@ -30,6 +32,6 @@ def makeResponse(res):
 	}
 	
 if __name__ == '__main__':
-	port = int(os.getenv('PORT', 5000))
-	print("Start app on port " % port)
-	app.run(debug=False, port=port, host='0.0.0.0')
+    port = int(os.getenv('PORT', 5000))
+    print("Starting app on port %d" % port)
+    app.run(debug=False, port=port, host='0.0.0.0')
