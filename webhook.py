1dd8391844d9debe7af985fb35496549760483d4
androrahul <39151779+androrahul@users.noreply.github.com>
2018-05-11 13:17:29 +0530

    changes

--- webhook.py
+++ webhook.py
@@ -25,123 +25,14 @@ def stock():
 def makeResponse(res):
 	#result = res.get("result");
 	speech = "This is news."
-	#return {
-	#"speech":speech,
-	#"displayText":speech,
-	#"source":"apiai-demostock-webhook"
-	#}
-	
-	#return {
-	#	"fulfillmentText": speech,
-	#	"fulfillmentMessages": [
-	#		{
-	#			"speech":speech
-	#		}
-	#	]
-	#}
-	
 	return {
-			"speech":speech,
-			"displayText":speech,		
-			"fulfillmentText": "This is a text response",
-			"fulfillmentMessages": [
-				  {
-					
-						"text": {
-						  "text": [
-							"lease tell me your first name"
-						  ]
-					
-				 }
-			  }
-			],
-			"source": "example.com",
-			"payload": {
-			  "google": {
-				"expectUserResponse": "true",
-				"richResponse": {
-				  "items": [
-					{
-					  "simpleResponse": {
-						"textToSpeech": "this is a simple response"
-					  }
-					}
-				  ]
-				}
-			  },
-			  "facebook": {
-				"text": "Hello, Facebook!"
-			  },
-			  "slack": {
-				"text": "This is a text response for Slack."
-			  }
-			},
-			"outputContexts": [
-			  {
-				"name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
-				"lifespanCount": "5",
-				"parameters": {
-				  "param": "param value"
-				}
-			  }
-			],
-			"followupEventInput": {
-			  "name": "event name",
-			  "languageCode": "en-US",
-			  "parameters": {
-				"param": "param value"
-			  }
-			}
-		}
+		"speech":speech,
+		"displayText":speech,
+		"contextOut":[],
+		"source":"apiai-demostock-webhook"
+	}
 	
 	
-	#return {
-	#	"speech": "this text is spoken out loud if the platform supports voice interactions",
-	#	"displayText": "this text is displayed visually",
-	#	"messages": {
-	#	  "type": 1,
-	#	  "title": "card title",
-	#	  "subtitle": "card text",
-	#	  "imageUrl": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png"
-	#	},
-	#	"data": {
-	#	  "google": {
-	#		"expectUserResponse": "true",
-	#		"richResponse": {
-	#		  "items": [
-	#			{
-	#			  "simpleResponse": {
-	#				"textToSpeech": "this is a simple response"
-	#			  }
-	#			}
-	#		  ]
-	#		}
-	#	  },
-	#	  "facebook": {
-	#		"text": "Hello, Facebook!"
-	#	  },
-	#	  "slack": {
-	#		"text": "This is a text response for Slack."
-	#	  }
-	#	},
-	#	"contextOut": [
-	#	  {
-	#		"name": "context name",
-	#		"lifespan": "5",
-	#		"parameters": {
-	#		  "param": "param value"
-	#		}
-	#	  }
-	#	],
-	#	"source": "example.com",
-	#	"followupEvent": {
-	#	  "name": "event name",
-	#	  "parameters": {
-	#		"param": "param value"
-	#	  }
-	#	}
-	#}
-	#
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
     print("Starting app on port %d" % port)
