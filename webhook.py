ac530245d568480299ddce5b9c02eaa9d570c997
androrahul <39151779+androrahul@users.noreply.github.com>
2018-05-11 10:26:30 +0530

    for v1

--- webhook.py
+++ webhook.py
@@ -25,11 +25,11 @@ def stock():
 def makeResponse(res):
 	#result = res.get("result");
 	speech = "This is news."
-	#return {
-	#"speech":speech,
-	#"displayText":speech,
-	#"source":"apiai-demostock-webhook"
-	#}
+	return {
+	"speech":speech,
+	"displayText":speech,
+	"source":"apiai-demostock-webhook"
+	}
 	
 	#return {
 	#	"fulfillmentText": speech,
@@ -40,62 +40,62 @@ def makeResponse(res):
 	#	]
 	#}
 	
-	return {
-			"fulfillmentText": "This is a text response",
-			"fulfillmentMessages": [
-			  {
-				"card": {
-				  "title": "card title",
-				  "subtitle": "card text",
-				  "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
-				  "buttons": [
-					{
-					  "text": "button text",
-					  "postback": "https://assistant.google.com/"
-					}
-				  ]
-				}
-			  }
-			],
-			"source": "example.com",
-			"payload": {
-			  "google": {
-				"expectUserResponse": "true",
-				"richResponse": {
-				  "items": [
-					{
-					  "simpleResponse": {
-						"textToSpeech": "this is a simple response"
-					  }
-					}
-				  ]
-				}
-			  },
-			  "facebook": {
-				"text": "Hello, Facebook!"
-			  },
-			  "slack": {
-				"text": "This is a text response for Slack."
-			  }
-			},
-			"outputContexts": [
-			  {
-				"name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
-				"lifespanCount": "5",
-				"parameters": {
-				  "param": "param value"
-				}
-			  }
-			],
-			"followupEventInput": {
-			  "name": "event name",
-			  "languageCode": "en-US",
-			  "parameters": {
-				"param": "param value"
-			  }
-			}
-		}
-	
+	#return {
+	#		"fulfillmentText": "This is a text response",
+	#		"fulfillmentMessages": [
+	#		  {
+	#			"card": {
+	#			  "title": "card title",
+	#			  "subtitle": "card text",
+	#			  "imageUri": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png",
+	#			  "buttons": [
+	#				{
+	#				  "text": "button text",
+	#				  "postback": "https://assistant.google.com/"
+	#				}
+	#			  ]
+	#			}
+	#		  }
+	#		],
+	#		"source": "example.com",
+	#		"payload": {
+	#		  "google": {
+	#			"expectUserResponse": "true",
+	#			"richResponse": {
+	#			  "items": [
+	#				{
+	#				  "simpleResponse": {
+	#					"textToSpeech": "this is a simple response"
+	#				  }
+	#				}
+	#			  ]
+	#			}
+	#		  },
+	#		  "facebook": {
+	#			"text": "Hello, Facebook!"
+	#		  },
+	#		  "slack": {
+	#			"text": "This is a text response for Slack."
+	#		  }
+	#		},
+	#		"outputContexts": [
+	#		  {
+	#			"name": "projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name",
+	#			"lifespanCount": "5",
+	#			"parameters": {
+	#			  "param": "param value"
+	#			}
+	#		  }
+	#		],
+	#		"followupEventInput": {
+	#		  "name": "event name",
+	#		  "languageCode": "en-US",
+	#		  "parameters": {
+	#			"param": "param value"
+	#		  }
+	#		}
+	#	}
+	#
 	
 if __name__ == '__main__':
     port = int(os.getenv('PORT', 5000))
