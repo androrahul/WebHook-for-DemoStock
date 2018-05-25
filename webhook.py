17ea11ede72b9e134f5e0bffcf7a71a769baa4be
androrahul <39151779+androrahul@users.noreply.github.com>
2018-05-25 11:25:08 +0530

    update

--- webhook.py
+++ webhook.py
@@ -77,7 +77,7 @@ def getLoginService(req):
 
 	obj = response.json()
 
-	response = 'Thank you user : ' + str(obj['id'])
+	temp = 'Thank you user : ' + str(obj['id'])
 
 	return commonResponse(temp)
 
